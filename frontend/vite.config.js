71c2727c172410f3f23c1dd95b806a528e9afb7d
Rushit Harsora <rushitharsora@gmail.com>
2024-10-28 18:56:24 +0530

    Update Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx', // Explicitly set JSX loader
-    include: /src\/.*\.jsx?$/, // Apply to .js and .jsx files in /src
-  },
 })
