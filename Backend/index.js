e97175dfe1521cbfc5561944d07440745d3e3b84
Rushit Harsora <rushitharsora@gmail.com>
2024-12-11 21:41:57 +0530

    Update Backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const mongoose = require('mongoose');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    // routes
+
+    app.use('/', (req, res) => {
+        res.send('Book  Server')
+    });
+}
+
+main().then(() => console.log("MongoDB connected Successfully")).catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
