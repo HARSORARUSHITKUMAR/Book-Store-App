40c9ff1a755d3ad88b68e3d91b46d9e7590fc4cd
Rushit Harsora <rushitharsora@gmail.com>
2024-12-14 14:55:27 +0530

    Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}))
 
+// routes
+const bookRoutes = require('./src/books/book.route');
+app.use("/api/books", bookRoutes)
 
 async function main() {
     await mongoose.connect(process.env.DB_URL);
-    // routes
-
     app.use('/', (req, res) => {
         res.send('Book  Server')
     });
@@ -20,4 +28,4 @@ main().then(() => console.log("MongoDB connected Successfully")).catch(err => co
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
---
+++ Backend/src/books/book.route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+// frontend => backend server => controller => book schema  => database => send to server => back to the frontend
+//post = when submit something fronted to db
+// get =  when get something back from db
+// put/patch = when edit or update something
+// delete = when delete something
+
+// post a book
+router.post("/create-book", async (req, res) => {
+    console.log(req.body);
+})
+
+
+module.exports = router;
