10a96a0346569113178cac4a9a83bba15a12ded1
Rushit Harsora <rushitharsora@gmail.com>
2024-12-27 03:18:23 +0530

    Complete

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://book-store-app-bar4.vercel.app"],
     credentials: true,
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
