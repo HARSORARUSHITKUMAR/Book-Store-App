aeedafb6ae777cdea0e352c0d5413b198ef02eb7
Rushit Harsora <rushitharsora@gmail.com>
2025-01-01 16:23:38 +0530

    Update With Backend Url

--- Backend/index.js
+++ Backend/index.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://book-store-app-three-lake.vercel.app"],
     credentials: true,
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 
 // routes
